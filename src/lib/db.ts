6b67c1b8e3905282c7b7e2cec9082b5d8514dfd9
kagepota <nelchrimson@gmail.com>
2026-04-26 19:09:08 +0800

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,11 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host:     process.env.DB_HOST || 'localhost',
-  user:     process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'resto_pos',
-  port:     parseInt(process.env.DB_PORT || '3306'),
+  host:     process.env.DB_HOST || 'countryside-db-nelchrimson-6a2f.c.aivencloud.com',
+  user:     process.env.DB_USER || 'avnadmin',
+  password: process.env.DB_PASS || 'AVNS_MQ2J92WoQddTUQAhcT1',
+  database: process.env.DB_NAME || 'defaultdb',
+  port:     parseInt(process.env.DB_PORT || '16619'),
   connectTimeout: 30000,
   waitForConnections: true,
   connectionLimit: 5,
